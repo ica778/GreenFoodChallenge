d149debaf4bf24fa707f444be696373198007886
Oscar <Jerald@users.noreply.github.com>
2018-10-21 22:09:53 -0700

    Initial commit

---
+++ app/src/main/java/com/soyiz/greenfoodchallenge/EcoActivity.java
@@ -0,0 +1,13 @@
+package com.soyiz.greenfoodchallenge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EcoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eco);
+    }
+}
