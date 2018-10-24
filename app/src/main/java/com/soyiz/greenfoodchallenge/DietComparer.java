64927285df769d73559067dd6b459d51f693db0c
ica29 <ica29@sfu.ca>
2018-10-23 23:58:56 -0700

    Added function to compare how well diet C02e compares to average

--- app/src/main/java/com/soyiz/greenfoodchallenge/DietComparer.java
+++ app/src/main/java/com/soyiz/greenfoodchallenge/DietComparer.java
@@ -1,5 +1,9 @@
 package com.soyiz.greenfoodchallenge;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /*Compares CO2e levels in two diets and calculates the % of CO2e saved with new diet
  * Calculates the difference in kg of CO2e between diets
  * Calculates the CO2e saved if all of Vancouver made similar changes
@@ -36,5 +40,36 @@ public class DietComparer {
         //2.31 kg of CO2 per L of gasoline burned
         float litresOfGasoline = (float)Math.round(CO2eDifference/2.31f);
         return litresOfGasoline;
+    }
+
+    // Returns a string describing how the CO2e in the diet compares to regional average
+    public String getHowWellUserComparesToRegion(Diet currentDiet) {
+        List<String> howWellDietC02eCompares = new ArrayList<>(Arrays.asList(
+                "Much better than regional average",
+                "Better than regional average",
+                "Average",
+                "Worse than average",
+                "Much worse than average"
+        ));
+        float currentDietC02e = currentDiet.getYearlyCO2e();
+        // 7.7 is C02e/ca for Vancouver according to lecture notes
+        float averageC02eInDietForVancouver =  7.7f;
+
+        // returns how well
+        if (currentDietC02e <= averageC02eInDietForVancouver * 0.75) {
+            return howWellDietC02eCompares.get(0);
+        }
+        else if (currentDietC02e <= averageC02eInDietForVancouver * 0.9) {
+            return howWellDietC02eCompares.get(1);
+        }
+        else if (currentDietC02e <= averageC02eInDietForVancouver * 1.1) {
+            return howWellDietC02eCompares.get(2);
+        }
+        else if (currentDietC02e <= averageC02eInDietForVancouver * 1.25) {
+            return howWellDietC02eCompares.get(3);
+        }
+        else {
+            return howWellDietC02eCompares.get(4);
+        }
     }
 }
