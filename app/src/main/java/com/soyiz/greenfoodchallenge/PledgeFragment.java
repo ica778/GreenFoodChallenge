870fc7fd9f405aeb4e051af557ee1bbba33309ca
Oscar <Jerald@users.noreply.github.com>
2018-10-28 23:48:59 -0700

    Added pledge and user fragments

---
+++ app/src/main/java/com/soyiz/greenfoodchallenge/PledgeFragment.java
@@ -0,0 +1,24 @@
+package com.soyiz.greenfoodchallenge;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PledgeFragment extends Fragment
+{
+    public PledgeFragment()
+    {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_pledge, container, false);
+    }
+
+}
