7b15554a51f9d8e9634b8b999b175c7f58d58e06
ica29 <ica29@sfu.ca>
2018-11-07 14:07:54 -0800

    updated pledge UI to use textview

--- app/src/main/java/com/soyiz/greenfoodchallenge/PledgeFragment.java
+++ app/src/main/java/com/soyiz/greenfoodchallenge/PledgeFragment.java
@@ -6,8 +6,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PledgeFragment extends Fragment {
+
+    private ListView pledgeListView;
 
     public PledgeFragment() {
         // Required empty public constructor
@@ -16,12 +24,16 @@ public class PledgeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_pledge, container, false);
+
+        List<String> listOfPledges = new ArrayList<String>();
+        ListAdapter pledgeAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, listOfPledges);
+        ListView listOfAllPledges = (ListView) view.findViewById(R.id.pledgeListView);
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_pledge, container, false);
     }
 
-    private void listOfPledges() {
-
-    }
 
 }
