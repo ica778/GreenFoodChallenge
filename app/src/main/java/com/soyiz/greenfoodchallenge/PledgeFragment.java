cfa8f393c63d2f8071cf187b990c0c46cf27e663
ica29 <ica29@sfu.ca>
2018-11-08 13:14:22 -0800

    clicking pledge has effect

--- app/src/main/java/com/soyiz/greenfoodchallenge/PledgeFragment.java
+++ app/src/main/java/com/soyiz/greenfoodchallenge/PledgeFragment.java
@@ -17,7 +17,7 @@ public class PledgeFragment extends Fragment {
     private ListView pledgeListView;
     private Spinner regionShowSpinner;
     private ArrayAdapter adapter;
-    private TextView spinnerMunicipalityText;
+    private TextView selectAPledgeText;
 
     public PledgeFragment() {
         // Required empty public constructor
@@ -25,28 +25,60 @@ public class PledgeFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
+                             Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_pledge, container, false);
 
         // Inflate the layout for this fragment
         initView(view);
+        spinnerActions(view);
+        populateListView();
         return view;
     }
 
     private void initView(View view) {
-        spinnerMunicipalityText = view.findViewById(R.id.showMunicipality);
-        adapter = ArrayAdapter.createFromResource(getActivity(), R.array.municipalities_spinner, android.R.layout.simple_spinner_item);
+        selectAPledgeText = view.findViewById(R.id.showMunicipality);
+        pledgeListView = (ListView) view.findViewById(R.id.listViewPledges);
+    }
+
+    private void spinnerActions(View view) {
+        adapter = ArrayAdapter.createFromResource(
+                getActivity(),
+                R.array.municipalities_spinner,
+                android.R.layout.simple_spinner_item);
         regionShowSpinner = (Spinner) view.findViewById(R.id.selectRegionSpinner);
         regionShowSpinner.setAdapter(adapter);
         regionShowSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            public void onItemSelected(AdapterView<?> parent, View viewClicked, int position, long id) {
                 String selectedSpinnerItem = (String) parent.getItemAtPosition(position);
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
 
+            }
+        });
+    }
+
+    private void populateListView() {
+        List<String> listOfPledges = new ArrayList<>();
+        listOfPledges.add("blue");
+        listOfPledges.add("red");
+        listOfPledges.add("green");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                getActivity(),
+                R.layout.list_view,
+                listOfPledges);
+        pledgeListView.setAdapter(adapter);
+        registerClickCallBackListView();
+    }
+
+    private void registerClickCallBackListView() {
+        pledgeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View viewClicked, int position, long id) {
+                selectAPledgeText.setText((String) parent.getItemAtPosition(position));
             }
         });
     }
