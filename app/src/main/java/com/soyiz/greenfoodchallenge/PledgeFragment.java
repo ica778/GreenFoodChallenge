e57c5ac8ab178b0ee74c3d59afea6b31349d68b8
ica29 <ica29@sfu.ca>
2018-11-08 13:45:48 -0800

    fixed problems with pledge ui

--- app/src/main/java/com/soyiz/greenfoodchallenge/PledgeFragment.java
+++ app/src/main/java/com/soyiz/greenfoodchallenge/PledgeFragment.java
@@ -10,7 +10,9 @@ import android.view.ViewGroup;
 import android.widget.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class PledgeFragment extends Fragment {
 
@@ -18,6 +20,9 @@ public class PledgeFragment extends Fragment {
     private Spinner regionShowSpinner;
     private ArrayAdapter adapter;
     private TextView selectAPledgeText;
+    private FirestoreHelper accessPledges = new FirestoreHelper();
+    private Map<String, Object> userPledgeInformation;
+    private List<String> listOfPledgesToShow;
 
     public PledgeFragment() {
         // Required empty public constructor
@@ -26,20 +31,23 @@ public class PledgeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_pledge, container, false);
 
-        // Inflate the layout for this fragment
         initView(view);
         spinnerActions(view);
         populateListView();
         return view;
     }
 
+    // assigns values to variables
     private void initView(View view) {
         selectAPledgeText = view.findViewById(R.id.showMunicipality);
         pledgeListView = (ListView) view.findViewById(R.id.listViewPledges);
+        listOfPledgesToShow = new ArrayList<String>();
     }
 
+    // Handles events on spinner
     private void spinnerActions(View view) {
         adapter = ArrayAdapter.createFromResource(
                 getActivity(),
@@ -50,7 +58,7 @@ public class PledgeFragment extends Fragment {
         regionShowSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View viewClicked, int position, long id) {
-                String selectedSpinnerItem = (String) parent.getItemAtPosition(position);
+                //listOfPledgesToShow.add(accessPledges.)
             }
 
             @Override
@@ -60,25 +68,23 @@ public class PledgeFragment extends Fragment {
         });
     }
 
+    // Put pledges on ListView
     private void populateListView() {
-        List<String> listOfPledges = new ArrayList<>();
-        listOfPledges.add("blue");
-        listOfPledges.add("red");
-        listOfPledges.add("green");
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(
                 getActivity(),
                 R.layout.list_view,
-                listOfPledges);
+                listOfPledgesToShow);
         pledgeListView.setAdapter(adapter);
         registerClickCallBackListView();
     }
 
+    // Handles click events on ListView
     private void registerClickCallBackListView() {
         pledgeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View viewClicked, int position, long id) {
-                selectAPledgeText.setText((String) parent.getItemAtPosition(position));
+                //selectAPledgeText.setText((String) parent.getItemAtPosition(position));
             }
         });
     }
