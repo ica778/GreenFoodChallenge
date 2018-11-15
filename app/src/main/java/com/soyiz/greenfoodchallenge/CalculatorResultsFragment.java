b5bc1554783052b7d58ed38a8ba6b9d99b5eb691
ianch <ica29@sfu.ca>
2018-11-15 00:14:02 -0800

    pie charts finished

--- app/src/main/java/com/soyiz/greenfoodchallenge/CalculatorResultsFragment.java
+++ app/src/main/java/com/soyiz/greenfoodchallenge/CalculatorResultsFragment.java
@@ -7,13 +7,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.PercentFormatter;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 
@@ -53,7 +57,6 @@ public class CalculatorResultsFragment extends Fragment {
                 (UserDietInfo.getInstance().getAmountOfProteinGrams("bean") * 4.1f) +
                 (UserDietInfo.getInstance().getAmountOfProteinGrams("vegetable") * 2) +
                 (UserDietInfo.getInstance().getAmountOfProteinGrams("egg") * 2));
-
         Float[] yData = {
                 (100 * (UserDietInfo.getInstance().getAmountOfProteinGrams("beef") * 27)) / totalC02eFootprintGrams,
                 (100 * (UserDietInfo.getInstance().getAmountOfProteinGrams("chicken") * 12.1f)) / totalC02eFootprintGrams,
@@ -64,41 +67,57 @@ public class CalculatorResultsFragment extends Fragment {
                 (100 * (UserDietInfo.getInstance().getAmountOfProteinGrams("egg") * 2)) / totalC02eFootprintGrams
         };
         String[] xData = {
-                "beef",
-                "chicken",
-                "pork",
-                "fish",
-                "bean",
-                "vegetable",
-                "egg"
+                "Beef",
+                "Chicken",
+                "Pork",
+                "Fish",
+                "Bean",
+                "Vegetable",
+                "Egg"
         };
 
         ArrayList<PieEntry> yEntries = new ArrayList<>();
-        ArrayList<String> xEntries = new ArrayList<>();
 
+        // Put values into entries that can be put into the pie chart
         for (int i = 0; i < yData.length; i++) {
-            if (yData[i] > 0) {
-                yEntries.add(new PieEntry(yData[i], i));
-            }
-        }
-        for (int i = 1; i < xData.length; i++) {
-            if (yData[i] > 0) {
-                xEntries.add( yData[i].toString() + " %\n" + xData[i]);
+            if(yData[i] > 0) {
+                yEntries.add(new PieEntry(yData[i], xData[i]));
             }
         }
 
-        PieDataSet dietC02ePercentsPieDataSet = new PieDataSet(yEntries, "C02e");
-        dietC02ePercentsPieDataSet.setSliceSpace(1);
-        dietC02ePercentsPieDataSet.setValueTextSize(15);
+        PieDataSet dietC02ePercentsPieDataSet = new PieDataSet(yEntries, "");
+
+        dietC02ePercentsPieChartView.animateY(1000, Easing.EasingOption.EaseInOutCubic);
+
+        // Set click events
         dietC02ePercentsPieChartView.setRotationEnabled(false);
         dietC02ePercentsPieChartView.setDrawHoleEnabled(false);
 
-        dietC02ePercentsPieChartView.getLegend().setEnabled(false);
+        // Set things outside of pie chart
+        Legend legend = dietC02ePercentsPieChartView.getLegend();
+        legend.setEnabled(true);
+        legend.setWordWrapEnabled(true);
+        legend.setPosition(Legend.LegendPosition.BELOW_CHART_CENTER);
         dietC02ePercentsPieChartView.getDescription().setText("");
 
+        // Set pie chart and its slices
+        dietC02ePercentsPieChartView.setUsePercentValues(true);
+        dietC02ePercentsPieDataSet.setSliceSpace(1f);
+        dietC02ePercentsPieDataSet.setColors(ColorTemplate.JOYFUL_COLORS);
+
+        // Set text in pie chart
         PieData pieData = new PieData(dietC02ePercentsPieDataSet);
-        dietC02ePercentsPieDataSet.setColors(ColorTemplate.VORDIPLOM_COLORS);
-        pieData.setValueTextSize(10f);
+        pieData.setValueFormatter(new PercentFormatter());
+
+        // Set y axis text color
+        dietC02ePercentsPieDataSet.setValueTextColor(Color.BLACK);
+        dietC02ePercentsPieDataSet.setValueTextSize(10f);
+
+        // Set x axis text color
+        dietC02ePercentsPieChartView.setEntryLabelColor(Color.BLACK);
+        dietC02ePercentsPieChartView.setEntryLabelTextSize(10f);
+
+        // Show pie chart
         dietC02ePercentsPieChartView.setData(pieData);
         dietC02ePercentsPieChartView.invalidate();
     }
@@ -115,40 +134,60 @@ public class CalculatorResultsFragment extends Fragment {
                 (UserDietInfo.getInstance().getAmountOfProteinGrams("egg") / totalProteinGrams) * 100
         };
         String[] xData = {
-                "beef",
-                "chicken",
-                "pork",
-                "fish",
-                "bean",
-                "vegetable",
-                "egg"
+                "Beef",
+                "Chicken",
+                "Pork",
+                "Fish",
+                "Bean",
+                "Vegetable",
+                "Egg"
         };
+
         ArrayList<PieEntry> yEntries = new ArrayList<>();
-        ArrayList<String> xEntries = new ArrayList<>();
 
+        // Put values into entries that can be put into the pie chart
         for (int i = 0; i < yData.length; i++) {
-            if (yData[i] > 0) {
-                yEntries.add(new PieEntry(yData[i], i));
-            }
-        }
-        for (int i = 1; i < xData.length; i++) {
-            if (yData[i] > 0) {
-                xEntries.add(xData[i]);
+            if(yData[i] > 0) {
+                yEntries.add(new PieEntry(yData[i], xData[i]));
             }
         }
 
-        PieDataSet dietProportionsPieDataSet = new PieDataSet(yEntries, "Proportions");
-        dietProportionsPieDataSet.setSliceSpace(1);
-        dietProportionsPieDataSet.setValueTextSize(15);
+        PieDataSet dietProportionsPieDataSet = new PieDataSet(yEntries, "");
+
+        dietProportionsPieChartView.animateY(1000, Easing.EasingOption.EaseInOutCubic);
+
+        // Set click events
         dietProportionsPieChartView.setRotationEnabled(false);
         dietProportionsPieChartView.setDrawHoleEnabled(false);
 
-        dietProportionsPieChartView.getLegend().setEnabled(false);
+        // Set things outside of pie chart
+        Legend legend = dietProportionsPieChartView.getLegend();
+        legend.setEnabled(true);
+        legend.setWordWrapEnabled(true);
+        legend.setPosition(Legend.LegendPosition.BELOW_CHART_CENTER);
         dietProportionsPieChartView.getDescription().setText("");
 
+        // Set pie chart and its slices
+        dietProportionsPieChartView.setUsePercentValues(true);
+        dietProportionsPieDataSet.setSliceSpace(1f);
+        dietProportionsPieDataSet.setColors(ColorTemplate.JOYFUL_COLORS);
+
+        // Set text in pie chart
         PieData pieData = new PieData(dietProportionsPieDataSet);
-        dietProportionsPieDataSet.setColors(ColorTemplate.VORDIPLOM_COLORS);
-        pieData.setValueTextSize(10f);
+        pieData.setValueFormatter(new PercentFormatter());
+
+        // Set y axis text color
+        dietProportionsPieDataSet.setValueTextColor(Color.BLACK);
+        dietProportionsPieDataSet.setValueTextSize(10f);
+
+        // Set x axis text color
+        dietProportionsPieChartView.setEntryLabelColor(Color.BLACK);
+        dietProportionsPieChartView.setEntryLabelTextSize(10f);
+
+        // Show x axis text
+        dietC02ePercentsPieChartView.setDrawEntryLabels(true);
+
+        // Show pie chart
         dietProportionsPieChartView.setData(pieData);
         dietProportionsPieChartView.invalidate();
     }
