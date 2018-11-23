63e1d61fc9b8ae399280be0a4e154ed16e3ee833
ica29 <ica29@sfu.ca>
2018-11-22 23:17:43 -0800

    added tests for Diet.java and ProteinSource.java

--- app/src/test/java/com/soyiz/greenfoodchallenge/DietTest.java
+++ app/src/test/java/com/soyiz/greenfoodchallenge/DietTest.java
@@ -10,68 +10,60 @@ import static org.junit.Assert.*;
 
 public class DietTest {
 
-
-
     @Test
-    public void loadFromStringMap() {
-        Map<ProteinSource, Float> proteinPercentMap = new HashMap<>();
-        proteinPercentMap.put(ProteinSource.Beef, 1.0f);
-        proteinPercentMap.put(ProteinSource.Chicken, 1.0f);
-        proteinPercentMap.put(ProteinSource.Vegetables, 1.0f);
-        proteinPercentMap.put(ProteinSource.Beans, 1.0f);
-        proteinPercentMap.put(ProteinSource.Eggs, 1.0f);
-        proteinPercentMap.put(ProteinSource.Pork, 1.0f);
-        proteinPercentMap.put(ProteinSource.Fish, 1.0f);
-        Map<String, Float> map = new HashMap<>();
-        map.put("test1", 1.0f);
-        map.put("test2", 1.0f);
-
-        for(Map.Entry<String, Float> entry : map.entrySet()) {
-            ProteinSource protein = ProteinSource.stringToEnumValue(entry.getKey());
-            assertEquals(null, protein);
-        }
-
-    }
-
-    @Test
-    public void exportToStringMap() {
-        Map<ProteinSource, Float> proteinPercentMap = new HashMap<>();
-        proteinPercentMap.put(ProteinSource.Beef, 1.0f);
-        proteinPercentMap.put(ProteinSource.Chicken, 1.0f);
-        proteinPercentMap.put(ProteinSource.Vegetables, 1.0f);
-        proteinPercentMap.put(ProteinSource.Beans, 1.0f);
-        proteinPercentMap.put(ProteinSource.Eggs, 1.0f);
-        proteinPercentMap.put(ProteinSource.Pork, 1.0f);
-        proteinPercentMap.put(ProteinSource.Fish, 1.0f);
-        Map<String, Float> outputMap = new HashMap<>();
-        for (Map.Entry<ProteinSource, Float> entry : proteinPercentMap.entrySet()) {
-            outputMap.put(entry.getKey().toString(), entry.getValue());
-        }
-
-        assertNotNull( outputMap);
-
-    }
-
-    @Test
-    public void setProteinPercent() {
+    public void setProteinPercentTest() {
         ProteinSource protein = ProteinSource.Beef;
         float percent = 1.0f;
         Map<ProteinSource, Float> proteinPercentMap = new HashMap<>();
         proteinPercentMap.put(protein, percent);
         assertEquals(1.0f, proteinPercentMap.get(protein), 0.1);
+
+        Diet dietTest = new Diet();
+        dietTest.setProteinPercent(ProteinSource.Beef, 10);
+        dietTest.setProteinPercent(ProteinSource.Pork, 10);
+        dietTest.setProteinPercent(ProteinSource.Fish, 10);
+        dietTest.setProteinPercent(ProteinSource.Chicken, 10);
+        dietTest.setProteinPercent(ProteinSource.Vegetables, 10);
+        dietTest.setProteinPercent(ProteinSource.Beans, 10);
+        dietTest.setProteinPercent(ProteinSource.Eggs, 40);
+
+        assertEquals(10.0, dietTest.getProteinPercent(ProteinSource.Beef), 0.1);
+        assertEquals(10.0, dietTest.getProteinPercent(ProteinSource.Pork), 0.1);
+        assertEquals(10.0, dietTest.getProteinPercent(ProteinSource.Fish), 0.1);
+        assertEquals(10.0, dietTest.getProteinPercent(ProteinSource.Chicken), 0.1);
+        assertEquals(10.0, dietTest.getProteinPercent(ProteinSource.Vegetables), 0.1);
+        assertEquals(10.0, dietTest.getProteinPercent(ProteinSource.Beans), 0.1);
+        assertEquals(40.0, dietTest.getProteinPercent(ProteinSource.Eggs), 0.1);
     }
 
     @Test
-    public void getProteinPercent() {
+    public void getProteinPercentTest() {
         ProteinSource protein = ProteinSource.Beef;
         float percent = 1.0f;
         Map<ProteinSource, Float> proteinPercentMap = new HashMap<>();
         proteinPercentMap.put(protein, percent);
         assertEquals(1.0f, proteinPercentMap.get(protein), 0.1);
+
+        Diet dietTest = new Diet();
+        dietTest.setProteinPercent(ProteinSource.Beef, 10);
+        dietTest.setProteinPercent(ProteinSource.Pork, 10);
+        dietTest.setProteinPercent(ProteinSource.Fish, 10);
+        dietTest.setProteinPercent(ProteinSource.Chicken, 10);
+        dietTest.setProteinPercent(ProteinSource.Vegetables, 10);
+        dietTest.setProteinPercent(ProteinSource.Beans, 10);
+        dietTest.setProteinPercent(ProteinSource.Eggs, 40);
+
+        assertEquals(10.0, dietTest.getProteinPercent(ProteinSource.Beef), 0.1);
+        assertEquals(10.0, dietTest.getProteinPercent(ProteinSource.Pork), 0.1);
+        assertEquals(10.0, dietTest.getProteinPercent(ProteinSource.Fish), 0.1);
+        assertEquals(10.0, dietTest.getProteinPercent(ProteinSource.Chicken), 0.1);
+        assertEquals(10.0, dietTest.getProteinPercent(ProteinSource.Vegetables), 0.1);
+        assertEquals(10.0, dietTest.getProteinPercent(ProteinSource.Beans), 0.1);
+        assertEquals(40.0, dietTest.getProteinPercent(ProteinSource.Eggs), 0.1);
     }
 
     @Test
-    public void getWeeklyCO2e() {
+    public void getWeeklyCO2eTest() {
         float output = 0;
         Map<ProteinSource, Float> proteinPercentMap = new HashMap<>();
         proteinPercentMap.put(ProteinSource.Beef, 1.0f);
@@ -83,19 +75,40 @@ public class DietTest {
         proteinPercentMap.put(ProteinSource.Fish, 1.0f);
 
         // Weekly serving in grams for protein
-        float weeklyServing = (float)ProteinSource.getDailyServing() * 7;
+        float weeklyServing = (float) ProteinSource.getDailyServing() * 7;
         for (Map.Entry<ProteinSource, Float> pair : proteinPercentMap.entrySet()) {
 
             // Weekly serving in grams for a specific protein
-            float proteinWeeklyServing = pair.getValue()/100 * weeklyServing;
+            float proteinWeeklyServing = pair.getValue() / 100 * weeklyServing;
 
             // Puts the serving into KG and multiplies by CO2e for the protein
             output += (proteinWeeklyServing / 1000f) * pair.getKey().getCO2e();
         }
         assertNotEquals(0.0f, output);
+
+        Diet dietTest = new Diet();
+        dietTest.setProteinPercent(ProteinSource.Beef, 10);
+        dietTest.setProteinPercent(ProteinSource.Pork, 10);
+        dietTest.setProteinPercent(ProteinSource.Fish, 10);
+        dietTest.setProteinPercent(ProteinSource.Chicken, 10);
+        dietTest.setProteinPercent(ProteinSource.Vegetables, 10);
+        dietTest.setProteinPercent(ProteinSource.Beans, 10);
+        dietTest.setProteinPercent(ProteinSource.Eggs, 40);
+
+        assertEquals(10.0, dietTest.getWeeklyCO2e(), 1.0);
     }
 
     @Test
-    public void getYearlyCO2e() {
+    public void getYearlyCO2eTest() {
+        Diet dietTest = new Diet();
+        dietTest.setProteinPercent(ProteinSource.Beef, 10);
+        dietTest.setProteinPercent(ProteinSource.Pork, 10);
+        dietTest.setProteinPercent(ProteinSource.Fish, 10);
+        dietTest.setProteinPercent(ProteinSource.Chicken, 10);
+        dietTest.setProteinPercent(ProteinSource.Vegetables, 10);
+        dietTest.setProteinPercent(ProteinSource.Beans, 10);
+        dietTest.setProteinPercent(ProteinSource.Eggs, 40);
+
+        assertEquals(514.0, dietTest.getYearlyCO2e(), 1.0);
     }
 }
